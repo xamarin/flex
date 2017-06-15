5142e62fc28cc2302c100e9e5fc4cedd19e15045
Laurent Sansonetti <lrz@hipbyte.com>
2017-06-15 21:59:44 +0200

    ditto but in row direction

--- tests/test.c
+++ tests/test.c
@@ -132,6 +132,7 @@ test_default_values2(void)
     // defaults to 0 for the main axis and the parent's size for the cross
     // axis.
     struct flex_item *root = flex_item_with_size(200, 200);
+    flex_item_set_direction(root, FLEX_DIRECTION_COLUMN);
 
     struct flex_item *child1 = flex_item_new();
     flex_item_set_width(child1, 100);
@@ -154,6 +155,40 @@ test_default_values2(void)
 
     TEST_EQUAL(flex_item_get_frame_width(child3), 200);
     TEST_EQUAL(flex_item_get_frame_height(child3), 0);
+
+    flex_item_free(root);
+}
+
+static void
+test_default_values3(void)
+{
+    // If the width/height property isn't set on a child, it's frame size
+    // defaults to 0 for the main axis and the parent's size for the cross
+    // axis.
+    struct flex_item *root = flex_item_with_size(200, 200);
+    flex_item_set_direction(root, FLEX_DIRECTION_ROW);
+
+    struct flex_item *child1 = flex_item_new();
+    flex_item_set_width(child1, 100);
+    flex_item_add(root, child1);
+
+    struct flex_item *child2 = flex_item_new();
+    flex_item_set_height(child2, 100);
+    flex_item_add(root, child2);
+
+    struct flex_item *child3 = flex_item_new();
+    flex_item_add(root, child3);
+
+    flex_layout(root);
+
+    TEST_EQUAL(flex_item_get_frame_width(child1), 100);
+    TEST_EQUAL(flex_item_get_frame_height(child1), 200);
+
+    TEST_EQUAL(flex_item_get_frame_width(child2), 0);
+    TEST_EQUAL(flex_item_get_frame_height(child2), 100);
+
+    TEST_EQUAL(flex_item_get_frame_width(child3), 0);
+    TEST_EQUAL(flex_item_get_frame_height(child3), 200);
 
     flex_item_free(root);
 }
@@ -2815,6 +2850,7 @@ main(void)
 {
     UNIT(default_values1);
     UNIT(default_values2);
+    UNIT(default_values3);
 
     UNIT(children1);
     UNIT(children2);
