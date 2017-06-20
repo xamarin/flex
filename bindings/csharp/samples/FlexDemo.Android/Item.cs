fa4313592723b91ff4b527ff12963041e2d51d03
Laurent Sansonetti <laurent.sansonetti@microsoft.com>
2017-06-20 21:40:10 +0200

    initial revision of Android sample

---
+++ bindings/csharp/samples/FlexDemo.Android/Item.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Widget;
+using Android.Views;
+
+using XF = Xamarin.Flex;
+
+namespace Xamarin.Flex.Android
+{
+    public class Item : XF.Item
+    {
+        public View view = null;
+
+        public new void Add(XF.Item child)
+        {
+            throw new NotSupportedException("use LayoutItem instead");
+        }
+		
+        public new void InsertAt(int index, XF.Item child)
+        {
+            throw new NotSupportedException("use LayoutItem instead");
+        }
+
+        public new XF.Item RemoveAt(int index)
+        {
+            throw new NotSupportedException("use LayoutItem instead");
+        }
+
+        public new void Layout()
+        {
+            throw new NotSupportedException("use LayoutItem instead");
+        }
+    }
+
+    public class LayoutItem : XF.Item
+    {
+        public FrameLayout view = null;
+
+        static private View ViewOfItem(XF.Item item)
+        {
+            if (item is Item) {
+                return ((Item)item).view;
+            }
+            if (item is LayoutItem) {
+                return ((LayoutItem)item).view;
+            }
+            throw new NotSupportedException("use Item or LayoutItem instead");
+        }
+
+        public new void Add(XF.Item child)
+        {
+            base.Add(child);
+            view.AddView(ViewOfItem(child));
+        }
+
+        public new void InsertAt(int index, XF.Item child)
+        {
+            base.InsertAt(index, child);
+            view.AddView(ViewOfItem(child), index);
+        }
+
+        public new XF.Item RemoveAt(int index)
+        {
+            var child = base.RemoveAt(index);
+            if (child != null) {
+                view.RemoveView(ViewOfItem(child));
+            }
+            return child;
+        }
+
+        public new void Layout()
+        {
+            base.Layout();
+            UpdateChildrenFrames(this);
+        }
+
+        static private void UpdateChildrenFrames(XF.Item parent)
+        {
+            foreach (XF.Item child in parent)
+            {
+                var view = ViewOfItem(child);
+                var lp = view.LayoutParameters;
+
+                var mlp = lp as ViewGroup.MarginLayoutParams;
+                if (mlp != null) {
+                    mlp.LeftMargin = (int)child.FrameX;
+                    mlp.TopMargin = (int)child.FrameY;
+                }
+                else {
+                    throw new NotSupportedException("child does not have margin layout params");
+                }
+
+                lp.Width = (int)child.FrameWidth;
+                lp.Height = (int)child.FrameHeight;
+
+                view.LayoutParameters = lp;
+
+                UpdateChildrenFrames(child);
+            }
+        }
+    }
+}
---
+++ bindings/csharp/samples/FlexDemo.Android/MainActivity.cs
@@ -0,0 +1,119 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Text;
+using Xamarin.Flex.Android;
+
+namespace FlexDemo.Android
+{
+    [Activity(Label = "FlexDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var metrics = new DisplayMetrics();
+            WindowManager.DefaultDisplay.GetMetrics(metrics);
+
+            float spacing = 40;
+
+            var root = new LayoutItem()
+            {
+                Width = metrics.WidthPixels,
+                Height = metrics.HeightPixels,
+                PaddingBottom = 80
+            };
+            root.view = new FrameLayout(this)
+            {
+                LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+            };
+
+            var label = new Item()
+            {
+                Margin = spacing
+            };
+            var label_view = new TextView(this)
+            {
+                Text = "This is the list of items you have added. You can add new items and clear the list using the buttons at the bottom. This label has extra lines on purpose.",
+            };
+            label.view = label_view;
+            label.Height = new StaticLayout(label_view.Text, label_view.Paint, (int)root.Width, Layout.Alignment.AlignNormal, 1, 0, true).Height;
+            root.Add(label);
+
+            var list = new Item()
+            {
+                Grow = 1,
+                MarginLeft = spacing,
+                MarginRight = spacing
+            };
+            var list_view = new ListView(this);
+            list_view.Adapter = new ArrayAdapter(this, Resource.Layout.TextViewItem);
+            list.view = list_view;
+            root.Add(list);
+
+            var input = new Item()
+            {
+                Margin = spacing
+            };
+            var input_view = new EditText(this);
+            input_view.Hint = "Enter list item";
+            input.Height = 80;
+            input.view = input_view;
+            root.Add(input);
+
+            var buttons_row = new LayoutItem()
+            {
+                Direction = Xamarin.Flex.Direction.Row,
+                Height = 80,
+                MarginLeft = spacing,
+                MarginRight = spacing,
+                MarginBottom = spacing
+            };
+            buttons_row.view = new FrameLayout(this)
+            {
+                LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+            };
+            root.Add(buttons_row);
+
+            var add_button = new Item()
+            {
+                Grow = 1,
+                Height = 80
+            };
+            var add_button_view = new Button(this);
+            add_button_view.Text = "Add";
+            add_button_view.Click += delegate
+            {
+                var adapter = (ArrayAdapter)list_view.Adapter;
+                if (input_view.Text != "") {
+                    adapter.Add(input_view.Text);
+                    input_view.Text = "";
+                }
+            };
+            add_button.view = add_button_view;
+            buttons_row.Add(add_button);
+
+            var clear_button = new Item()
+            {
+                Grow = 1,
+                Height = 80
+            };
+            var clear_button_view = new Button(this);
+            clear_button_view.Text = "Clear";
+            clear_button_view.Click += delegate
+            {
+                var adapter = (ArrayAdapter)list_view.Adapter;
+                adapter.Clear();
+            };
+            clear_button.view = clear_button_view;
+            buttons_row.Add(clear_button);
+
+            root.Layout();
+
+            SetContentView(root.view);
+        }
+    }
+}
