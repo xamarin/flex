c696265356826bcf54a3d74e75eeec8d13d4a191
Laurent Sansonetti <lrz@hipbyte.com>
2017-09-26 23:37:13 +0200

    initial revision of tests for the SelfSizing property

---
+++ bindings/csharp/tests/test_self_sizing.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See the LICENSE.txt file in the project root
+// for the license information.
+
+using System;
+using Xamarin.Flex;
+
+public partial class Test
+{
+    void test_self_sizing()
+    {
+        Item root = new Item(500, 500);
+
+        Item item = new Item(10, 20);
+        item.SelfSizing = delegate(Item _, ref float width, ref float height) {
+            width += 100;
+            height += 100;
+        };
+        root.Add(item);
+
+        root.Layout();
+
+        assert(item.FrameWidth == 110);
+        assert(item.FrameHeight == 120);
+    }
+}
