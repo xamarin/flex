9cbb4a28aa29064e8e211207d59201f2d7f7cede
Laurent Sansonetti <laurent.sansonetti@microsoft.com>
2017-09-29 17:55:32 +0200

    add test to ensure that the self_sizing callback is always called during layout

--- tests/test_self_sizing.c
+++ tests/test_self_sizing.c
@@ -2,20 +2,20 @@
 // Licensed under the MIT License. See the LICENSE.txt file in the project root
 // for the license information.
 
-static bool dummy_self_sizing_called;
+static int dummy_self_sizing_called;
 static float dummy_self_sizing_size[2];
 
 static void
 dummy_self_sizing(struct flex_item *item, float size[2])
 {
-    dummy_self_sizing_called = true;
+    dummy_self_sizing_called++;
     dummy_self_sizing_size[0] = size[0];
     dummy_self_sizing_size[1] = size[1];
 }
 
 #define DUMMY_SELF_SIZING_RESET() \
     do { \
-        dummy_self_sizing_called = false; \
+        dummy_self_sizing_called = 0; \
         dummy_self_sizing_size[0] = 0; \
         dummy_self_sizing_size[1] = 0; \
     } \
@@ -50,7 +50,7 @@ test_self_sizing_dummy1(void)
 
     flex_layout(root);
 
-    TEST_EQUAL(dummy_self_sizing_called, true);
+    TEST_EQUAL(dummy_self_sizing_called, 1);
     TEST_EQUAL(dummy_self_sizing_size[0], 100);
     TEST_EQUAL(dummy_self_sizing_size[1], 0);
 
@@ -71,7 +71,7 @@ test_self_sizing_dummy2(void)
 
     flex_layout(root);
 
-    TEST_EQUAL(dummy_self_sizing_called, true);
+    TEST_EQUAL(dummy_self_sizing_called, 1);
     TEST_EQUAL(dummy_self_sizing_size[0], 50);
     TEST_EQUAL(dummy_self_sizing_size[1], 0);
 
@@ -92,7 +92,7 @@ test_self_sizing_dummy3(void)
 
     flex_layout(root);
 
-    TEST_EQUAL(dummy_self_sizing_called, true);
+    TEST_EQUAL(dummy_self_sizing_called, 1);
     TEST_EQUAL(dummy_self_sizing_size[0], 100);
     TEST_EQUAL(dummy_self_sizing_size[1], 50);
 
@@ -114,7 +114,7 @@ test_self_sizing_dummy4(void)
 
     flex_layout(root);
 
-    TEST_EQUAL(dummy_self_sizing_called, true);
+    TEST_EQUAL(dummy_self_sizing_called, 1);
     TEST_EQUAL(dummy_self_sizing_size[0], 50);
     TEST_EQUAL(dummy_self_sizing_size[1], 50);
 
@@ -135,9 +135,30 @@ test_self_sizing_dummy5(void)
 
     flex_layout(root);
 
-    TEST_EQUAL(dummy_self_sizing_called, true);
+    TEST_EQUAL(dummy_self_sizing_called, 1);
     TEST_EQUAL(dummy_self_sizing_size[0], 100);
     TEST_EQUAL(dummy_self_sizing_size[1], 0);
+
+    flex_item_free(root);
+}
+
+void
+test_self_sizing_dummy6(void)
+{
+    struct flex_item *root = flex_item_with_size(100, 100);
+
+    struct flex_item *child = flex_item_new();
+    flex_item_set_self_sizing(child, dummy_self_sizing);
+    flex_item_add(root, child);
+
+    DUMMY_SELF_SIZING_RESET();
+
+    for (int i = 0; i < 42; i++) {
+        flex_layout(root);
+        TEST_EQUAL(dummy_self_sizing_called, i + 1);
+    }
+
+    TEST_EQUAL(dummy_self_sizing_called, 42);
 
     flex_item_free(root);
 }
