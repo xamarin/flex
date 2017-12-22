01c2828cd52395b19b39e908c0d0c4def6b5c127
Rui Marinho <me@ruimarinho.net>
2017-12-22 17:18:59 +0000

    [Test] Add unit test csproj

---
+++ bindings/csharp/Xamarin.Flex.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xamarin.Flex.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xamarin.Flex.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed1c56e6-d78c-4f9a-a2c2-e0622fcf70f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ bindings/csharp/Xamarin.Flex.UnitTests/XamarinFlexTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xamarin.Flex.UnitTests
+{
+	[TestClass]
+	public class XamarinFlexTests
+	{
+		[TestMethod]
+		public void TestFlex()
+		{
+			var item = new Xamarin.Flex.Item(22, 22);
+			Assert.IsNotNull(item);
+		}
+	}
+}
